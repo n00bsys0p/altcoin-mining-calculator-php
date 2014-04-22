bb6c2f61bf26136502ee1ccf2e91ff934e3a24d2
Alex Shepherd <n00b@n00bsys0p.co.uk>
2014-04-23 00:35:38 +0100

    Added Logicoin, as it's an up and coming X11 coin.

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ require_once(APP_DIR . '/subsidy_functions/HirocoinSubsidyFunction.php');
 require_once(APP_DIR . '/subsidy_functions/DarkcoinSubsidyFunction.php');
 require_once(APP_DIR . '/subsidy_functions/LimecoinSubsidyFunction.php');
 require_once(APP_DIR . '/subsidy_functions/QuebecoinSubsidyFunction.php');
+require_once(APP_DIR . '/subsidy_functions/LogicoinSubsidyFunction.php');
 
 $app = new \n00bsys0p\CalculatorApp($config);
 
---
+++ subsidy_functions/LogicoinSubsidyFunction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace n00bsys0p;
+
+require_once(APP_DIR . '/subsidy_functions/interfaces/SubsidyFunctionInterface.php');
+
+/**
+ * Logicoin subsidy function
+ */
+class LogicoinSubsidyFunction implements SubsidyFunctionInterface
+{
+    /**
+     * Logicoin's subsidy function ported from GetBlockValue
+     *
+     * @param  integer $nHeight The block height for which to find the subsidy
+     * @param  integer $dDiff   The given block's difficulty
+     * @return float
+     */
+    public function getBlockValue($nHeight, $dDiff)
+    {
+        $nSubsidy = 50;
+
+        // Susidy is cut in half every 43200 block, Which is about every 30 days.
+        $nSubsidy >>= ($nHeight / 43200);
+
+        return $nSubsidy;
+    }
+}
